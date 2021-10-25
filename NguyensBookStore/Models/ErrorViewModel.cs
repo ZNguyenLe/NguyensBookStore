a68c5da6786493914d4522b7a159b885209cd387
Nguyen Le <Nguyen.Le@stclairconnect.ca>
2021-10-25 14:47:41 -0400

    Add project files.

---
+++ NguyensBookStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NguyensBookStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
